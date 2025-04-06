42fcd0a139f1cb3b3f757f59f1a31e0c7ec1590f
Vedant Salke <vedantsalke17@gmail.com>
2025-04-07 00:11:03 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import VideoCoferencing from './component/VideoConferencing.jsx'
+function App() {
+ 
+
+  return (
+    <>
+    <h1 className="video">Video Conferecning</h1>
+    <VideoCoferencing/>
+
+    
+    </>
+  )
+}
+
+export default App
